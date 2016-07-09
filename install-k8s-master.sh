0d7e8966691ba9c3117a06d070f07796ec0c6b86
Andrew Battye <andrew.battye@sap.com>
2016-07-09 10:27:44 +0100

    manifests

--- install-k8s-master.sh
+++ install-k8s-master.sh
@@ -6,6 +6,8 @@ apt-get install bridge-utils
 
 echo "Copying kubernetes service configuration files"
 mkdir /etc/kubernetes
+mkdir /etc/kubernetes/manifests-multi
+
 cp -f ./rootfs/etc/kubernetes/k8s.conf /etc/kubernetes/k8s.conf
 cp -f ./rootfs/lib/systemd/system/docker-bootstrap.service /lib/systemd/system/docker-bootstrap.service
 cp -f ./rootfs/lib/systemd/system/docker-bootstrap.socket /lib/systemd/system/docker-bootstrap.socket
@@ -14,6 +16,12 @@ cp -f ./rootfs/lib/systemd/system/k8s-flannel.service /lib/systemd/system/k8s-fl
 cp -f ./rootfs/lib/systemd/system/docker.service /lib/systemd/system/docker.service
 cp -f ./rootfs/lib/systemd/system/docker.socket /lib/systemd/system/docker.socket
 cp -f ./rootfs/lib/systemd/system/k8s-master.service /lib/systemd/system/k8s-master.service
+
+cp -f ./conf/kube-api-server.yaml /etc/kubernetes/manifests-multi
+cp -f ./conf/kube-manager.yaml /etc/kubernetes/manifests-multi
+cp -f ./conf/kube-proxy.yaml /etc/kubernetes/manifests-multi
+cp -f ./conf/kube-scheduler.yaml /etc/kubernetes/manifests-multi
+
 
 echo "Reloading the system service configuration"
 systemctl daemon-reload
